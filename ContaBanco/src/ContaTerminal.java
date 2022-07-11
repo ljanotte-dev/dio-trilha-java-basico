26d51efa3b2ad1946f80edf4e041e71ce01fd739
Leandro Janotte <ljanotte.dev@gmail.com>
2022-07-11 00:10:22 -0300

    Renomear Projeto

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // Fazer:
+        // * Conhecer e importar a classe Scanner;
+        // * Exibir as mensagens para o nosso usuário;
+        // * Obter pela classe scanner os valores digitados no terminla;
+        // * Exibir a mensagem conta criada;
+
+        double saldo = 25;
+        double valorSolicitado = 18;
+        
+
+    }
+}
